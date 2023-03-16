9fd20b09d3bfd989d436a703ec9ca10bbbe50c46
Patriks <patriks.venens@gmail.com>
2023-03-16 10:00:42 +0200

    Izzveidots Java projekts

---
+++ src/Pizzeria.java
@@ -0,0 +1,17 @@
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import UI.Maneger;
+
+public class Pizzeria {
+	public static void main(String[] args) {
+		Maneger manager= new Maneger();		
+
+	}
+
+}
