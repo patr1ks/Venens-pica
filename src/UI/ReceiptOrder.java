2b7cf4aa97580f0ad743f140b0ffe3bda2df95c9
Patriks <patriks.venens@gmail.com>
2023-03-16 10:14:57 +0200

    Izveidota klase lai apskatītu pasūtijumus

---
+++ src/UI/ReceiptOrder.java
@@ -0,0 +1,27 @@
+package UI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ReceiptOrder extends JPanel {
+	Maneger maneger;
+	ReceiptOrder(Maneger maneger){
+		this.maneger = maneger;
+		this.setLayout(null);
+		JButton backButton  = new JButton("Atpakaļ");
+		backButton.setBounds(210, 270, 80, 25);
+		backButton.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				maneger.showMainMenu();
+				
+			}
+			
+		});
+		this.add(backButton);
+	}
+}
