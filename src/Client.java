568d2dee5c41fe00dd9eda641c61e9413beb5cb9
Patriks <patriks.venens@gmail.com>
2023-03-16 10:15:50 +0200

    Izveidota kur saglabās klienta informāciju

---
+++ src/Client.java
@@ -0,0 +1,25 @@
+
+public class Client {
+	
+	String Name;
+	String Email;
+	String Number;
+
+	public Client(String Name, String Number, String Email) {
+		Name = this.Name;
+		Email = this.Email;
+		Number = this.Number;
+		
+	}
+	public String getName() {
+		return Name;
+	}
+	
+	public String getEmail() {
+		return Email;
+	}
+	
+	public String getNumber() {
+		return Number;
+	}
+}
